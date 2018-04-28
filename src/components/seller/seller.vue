4d6a081dc692ecfbc2af77f339ce6598508832d5
guilishabai <guilishabai@126.com>
2018-04-28 15:27:02 +0800

    seller

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -28,29 +28,29 @@
             </div>
           </li>
         </ul>
-        <div class="favorite">
-          <span class="icon-favorite"></span>
-          <span class="text"></span>
+        <div class="favorite" @click="toggleFavorite">
+          <span class="icon-favorite" :class="{'active': favorite}"></span>
+          <span class="text">{{favoriteText}}</span>
         </div>        
       </div>
       <split></split>
       <div class="bulletin">
         <h1 class="title">公告与活动</h1>
         <div class="content-wrapper border-1px">
-          <p class="content">啊科技时代哈克的</p>
+          <p class="content">{{seller.bulletin}}</p>
         </div>
         <ul v-if="seller.supports" class="supports">
           <li class="support-item border-1px" v-for="(item, index) in seller.supports" :key="index">
-            <span class="icon"></span>
-            <span class="text">啊实打</span>
+            <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+            <span class="text">{{seller.supports[index].description}}</span>
           </li>
         </ul>                
       </div>
       <split></split>
       <div class="pics">
         <h1 class="title">商家实景</h1>
-        <div class="pic-wrapper">
-          <ul class="pic-list">
+        <div class="pic-wrapper" ref="picWrapper">
+          <ul class="pic-list" ref="picList">
             <li class="pic-item" v-for="(pic, index) in seller.pics" :key="index">
               <img :src="pic" width="120" height="90">
             </li>
@@ -70,7 +70,7 @@
 </template>
 <script>
 import BetterScroll from 'better-scroll'
-// import {saveToLocal, loadFromLocal} from 'common/js/store'
+import {saveToLocal, loadFromLocal} from 'common/js/store'
 import star from 'components/star/star'
 import split from 'components/split/split'
 export default {
@@ -81,6 +81,62 @@ export default {
   props: {
     seller: {
       type: Object
+    }
+  },
+  data() {
+    return {
+      favorite: (() => {
+        return loadFromLocal(this.seller.id, 'favorite', false)
+      })()
+    }
+  },
+  created() {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+  },
+  computed: {
+    favoriteText() {
+      return this.favorite ? '已收藏' : '收藏'
+    }
+  },
+  methods: {
+    toggleFavorite(e) {
+      this.favorite = !this.favorite
+      saveToLocal(this.seller.id, 'favorite', this.favorite)
+    },
+    _initScroll() {
+      if (!this.scroll) {
+        this.$nextTick(() => {
+          this.scroll = new BetterScroll(this.$refs.seller, {
+            click: true
+          })
+        })
+      } else {
+        this.scroll.refresh()
+      }
+    },
+    _initPics() {
+      if (this.seller.pics) {
+        let picWidth = 120
+        let margin = 6
+        let width = (picWidth + margin) * this.seller.pics.length - margin
+        this.$refs.picList.style.width = width + 'px'
+        this.$nextTick(() => {
+          if (!this.picScroll) {
+            this.picScroll = new BetterScroll(this.$refs.picWrapper, {
+              scrollX: true,
+              eventPassthrough: 'vertical'
+            })
+          } else {
+            this.picScroll.refresh()
+          }
+        })
+      }
+    }
+  },
+  watch: {
+    'seller'() {
+      this._initScroll()
+      this._initPics()
     }
   }
 }
@@ -94,7 +150,7 @@ export default {
     bottom: 0
     left: 0
     width: 100%
-    // overflow: hidden
+    overflow: hidden
     .overview{
       position: relative
       padding: 18px
